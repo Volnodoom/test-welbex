d7667fbf1d68526ab7726097c52e19996a1bd98b
Alexander <bdiyoz@gmail.com>
2023-03-17 14:13:39 +0300

    FEAT: add global styles, update jsconfig

---
+++ src/themes/default-theme.js
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  color: {
+    black: '#0E1014',
+    white: '#E4E5EA',
+    whiteActive: '#EBDDCC',
+    blue: '#4077F3',
+    blueActive: '#2E34DB',
+    grey: '#0E1014',
+  }
+};
